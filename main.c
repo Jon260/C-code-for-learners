66f2f3bfc7d88d090d5d827f40eba77f2b32dcc0
Jon260 <77447764+Jon260@users.noreply.github.com>
2024-12-02 17:22:16 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include  <stdio.h> /*input output incluadition program*/
+
+
+int main() /*Main Fuction*/
+{
+ float num1, num2, num3; /*float veriable*/
+
+ printf("Enter Number: "); /*printf statement for Entering a number*/
+ scanf("\t%f",&num1); /*Scaning number*/
+
+
+ printf("Enter Number: ");  /*printf statement for Entering a number*/
+ scanf("\t%f",&num2); /*Scaning number*/
+
+
+ printf("Enter Number: ");  /*printf statement for Entering a number*/
+ scanf("\t%f",&num3); /*Scaning number*/
+
+ float total = num1 + num2 + num3; /*A float varible that gathering the numbers to gether */
+
+ printf("Total:\t%f\n",  total); /*printing the final result */
+
+ 
+}
+
+
+
